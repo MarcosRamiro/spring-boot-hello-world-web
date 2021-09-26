9000ffd8bcc251b00185693f59d0d107c858891a
Marcos Ramiro <mrsantos2006@gmail.com>
2021-09-26 01:18:34 -0300

    ajustes entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
+result=$(wget "https://swapi.dev/api/${PATH_1}/${PATH_2}")
+echo "Response from server"
+echo $result
+
+SERVER_SERVLET_CONTEXT_PATH=${SERVER_SERVLET_CONTEXT_PATH}
+
+printenv
+
 JAVA_OPTS="-Dserver.port=${SERVER_PORT} \
- -Dserver.servlet.context-path=/${SERVER_SERVLET_CONTEXT_PATH} \
+ -Dserver.servlet.context-path=/$SERVER_SERVLET_CONTEXT_PATH \
  -Daws.asg=${AWS_ASG} "
+
 echo $JAVA_OPTS
+
 exec java $JAVA_OPTS -jar /app/app.jar
