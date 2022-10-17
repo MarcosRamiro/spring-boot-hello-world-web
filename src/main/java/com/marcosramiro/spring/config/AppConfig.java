5826748abcbd318a54992d80ae8187b7b2b4fb76
Marcos Ramiro <mrsantos2006@gmail.com>
2022-10-17 00:04:13 -0300

    fix remoção do kafka

--- src/main/java/com/marcosramiro/spring/config/AppConfig.java
+++ src/main/java/com/marcosramiro/spring/config/AppConfig.java
@@ -1,14 +1,7 @@
 package com.marcosramiro.spring.config;
 
 import java.lang.reflect.Proxy;
-import java.util.Properties;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
